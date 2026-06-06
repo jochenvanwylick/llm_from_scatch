27484657491657de356ea826bf3bb41c1fa1957e
Jochen van Wylick <jochenvanwylick@hotmail.com>
2026-06-06 21:05:00 +0200

    Moved files around

---
+++ src/python/main.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn.functional as F
+
+
+def main():
+    y = torch.tensor([1.0])
+    x1 = torch.tensor([1.1])
+
+    w1 = torch.tensor([2.2])
+    b = torch.tensor([0.0])
+    z = x1 * w1 + b
+    a = torch.sigmoid(z)
+    loss = F.binary_cross_entropy(a, y)
+
+    print(loss)
+    print("whatever")
+
+
+if __name__ == "__main__":
+    main()

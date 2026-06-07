76f3e1038a48ccaf6fd2bb3d0ef0d0247b1c637e
Jochen van Wylick <jochenvanwylick@hotmail.com>
2026-06-07 15:16:03 +0200

    tokenizer v1 - with assertions for similarity before / after

--- src/python/scratch.py
+++ src/python/scratch.py
@@ -1,6 +1,8 @@
 import logging
 import re
 from rich.logging import RichHandler
+from tokenizer import SimpleTokenizerV1
+from rapidfuzz import fuzz
 
 # Setup logging
 logging.basicConfig(
@@ -17,53 +19,26 @@ with open(training_file, "r", encoding="utf-8") as f:
 log.info(f"Training file {training_file} read - {len(raw_text)} characters")
 
 # Use regex to split - extracting words - which will make up the dictionary later
-preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+preprocessed: list[str] = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
 preprocessed = [item.strip() for item in preprocessed if item.strip()]
 log.info(f"Split text into {len(preprocessed)} words")
 
-all_words = sorted(set(preprocessed))
+all_words: list[str] = sorted(set(preprocessed))
 vocab_size = len(all_words)
 log.info(f"Vocabulary size {len(all_words)}")
 
-
-vocab = {token: integer for integer, token in enumerate(all_words)}
+vocab: dict[str, int] = {token: integer for integer, token in enumerate(all_words)}
 for i, item in enumerate(vocab.items()):
     log.info(f"Vocab entry: {item}")
     if i >= 5:
         break
 
-
-class SimpleTokenizerV1:
-    def __init__(self, vocab) -> None:
-        self.str_to_int = vocab
-        self.int_to_str = {i: s for s, i in vocab.items()}
-
-    def encode(self, text):
-        # Use regex to split - extracting words - which will make up the dictionary later
-        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
-        preprocessed = [item.strip() for item in preprocessed if item.strip()]
-        ids = [self.str_to_int[s] for s in preprocessed]
-
-        log.info(f"Split text into {len(preprocessed)} words")
-        return ids
-
-    def decode(self, ids):
-        text = " ".join([self.int_to_str[i] for i in ids])
-
-        text = re.sub(r'\s+([,.?!"()\'])', r"\1", text)
-        return text
-
-
 t = SimpleTokenizerV1(vocab)
-text = """"It's the last he painted, you know,"
-    Mrs. Gisburn said with pardonable pride. """
-ids = t.encode(text)
-log.info(f"Encoding of:\n{text}\nIs equal to: {ids}")
-decoded = t.decode(ids)
-log.info(f"Decoded:\n{decoded}")
+text = (
+    """"It's the last he painted, you know, Mrs. Gisburn said with pardonable pride."""
+)
+encoded: list[int] = t.encode(text)
+decoded: str = t.decode(encoded)
 
-
-log.info(text.strip())
-log.info(decoded.strip())
-assert decoded.strip() == text.strip(), "Decoded text should match encoded text"
-assert len(ids) == 14, "Expecting 14 encoded tokens"
+ratio: float = fuzz.ratio(text, decoded)
+assert ratio > 0.95, "expecting orginal to VERY similar to original>encoded>decoded"
---
+++ src/python/tokenizer.py
@@ -0,0 +1,22 @@
+import re
+
+
+class SimpleTokenizerV1:
+    def __init__(self, vocab: dict[str, int]) -> None:
+        self.str_to_int: dict[str, int] = vocab
+        self.int_to_str: dict[int, str] = {i: s for s, i in vocab.items()}
+
+    def encode(self, text: str):
+        # Use regex to split - extracting words - which will make up the dictionary later
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        ids = [self.str_to_int[s] for s in preprocessed]
+
+        return ids
+
+    def decode(self, ids: list[int]):
+        text = " ".join([self.int_to_str[i] for i in ids])
+
+        text = re.sub(r'\s+([,.?!"()\'])', r"\1", text).lstrip()
+        return text
+

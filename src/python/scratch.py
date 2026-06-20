f7279c085791673de94c9df066e29731ed065b5c
Jochen van Wylick <jochenvanwylick@hotmail.com>
2026-06-20 07:13:12 +0200

    Tiktoken

--- src/python/scratch.py
+++ src/python/scratch.py
@@ -1,8 +1,11 @@
 import logging
 import re
 from rich.logging import RichHandler
-from tokenizer import SimpleTokenizerV1
+from tokenizer import SimpleTokenizerV1, SimpleTokenizerV2
 from rapidfuzz import fuzz
+from importlib.metadata import version
+import tiktoken
+
 
 # Setup logging
 logging.basicConfig(
@@ -42,3 +45,33 @@ decoded: str = t.decode(encoded)
 
 ratio: float = fuzz.ratio(text, decoded)
 assert ratio > 0.95, "expecting orginal to VERY similar to original>encoded>decoded"
+
+all_tokens = sorted(list(set(preprocessed)))
+all_tokens.extend(["<|endoftext|>", "<|unk|>"])
+vocab = {token: integer for integer, token in enumerate(all_tokens)}
+
+print(len(vocab.items()))
+
+for i, item in enumerate(list(vocab.items())[-5:]):
+    print(item)
+
+t1 = SimpleTokenizerV2(vocab)
+text1 = "Hello, do you like tea?"
+text2 = "In the sunlit terraces of the palace."
+text = " <|endoftext|> ".join((text1, text2))
+print(text)
+
+print(t1.encode(text))
+
+print("tiktoken version: ", version("tiktoken"))
+
+tokenizer = tiktoken.get_encoding("gpt2")
+
+text = (
+    "Hello, do you like tea? <|endoftext|> In the sunlit terracesof some unknownPlace."
+)
+
+integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+print(integers)
+strings = tokenizer.decode(integers)
+print(strings)
